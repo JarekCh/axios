8a12404f83617a72fd809187815dd6bcf6857b41
Jaroslaw Charchula <charchula@gmail.com>
2022-07-18 18:52:48 +0200

    edit fist req with get

--- src/examples/1-first-request.js
+++ src/examples/1-first-request.js
@@ -1,10 +1,21 @@
 import { useEffect } from 'react';
+import axios from 'axios';
 // limit, if 429 wait for 15 min and try again
 const url = 'https://course-api.com/react-store-products';
 
 const FirstRequest = () => {
+  const fetchData = async () => {
+    try {
+      const respone = await axios.get(url);
+      const data = respone.data;
+      console.log(respone.data);
+
+    } catch(err) {
+      console.log(err.respone);
+    }
+  }
   useEffect(() => {
-    console.log('first axios request');
+    fetchData();
   }, []);
 
   return <h2 className='text-center'>first request</h2>;
